2cdda8c0e9bcb55b8f91b8e90c4f36f6af3f842d
Mihail <mihail.larionow2001@gmail.com>
2024-03-09 21:45:50 +0300

    Add ViewModel class

---
+++ app/src/main/java/com/michel/productsapp/presentation/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.michel.productsapp.presentation
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    init{
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
